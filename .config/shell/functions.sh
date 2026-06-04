1573ceec20931ff04c4dc4a2ceb19d60d44962f5
Taylor Viti <tviti@uoregon.edu>
2026-06-04 02:12:27 -0700

    Initial commit

---
+++ .config/shell/functions.sh
@@ -0,0 +1,19 @@
+e() { emacsclient -nw "$@"; }
+
+proj() {
+  local dir
+  dir=$(find ~/Source -maxdepth 3 -name ".git" -type d 2>/dev/null \
+        | sed 's|/.git$||' \
+        | fzf --preview 'ls {}')
+  [ -n "$dir" ] && cd "$dir"
+}
+
+pf() {
+  local file
+  file=$(git ls-files | fzf --preview 'cat {}')
+  [ -n "$file" ] && ${EDITOR} "$file"
+}
+
+groot() {
+  cd "$(git rev-parse --show-toplevel)"
+}
