8013d6c4dd82020888ff970045b1ab0848c6574e
Taylor Viti <tviti@uoregon.edu>
2026-07-15 14:42:55 -0700

    Add aliases.sh and init.sh loader

---
+++ .config/shell/aliases.sh
@@ -0,0 +1,21 @@
+# Navigation
+alias ..='cd ..'
+alias ...='cd ../..'
+alias -- -='cd -'
+
+# git
+alias gs='git status'
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias ga='git add'
+alias gc='git commit'
+alias gco='git checkout'
+alias gb='git branch'
+alias gl='git log --oneline --graph --decorate -20'
+alias gp='git push'
+alias gpl='git pull'
+
+# ls
+alias ll='ls -lh'
+alias la='ls -lAh'
+alias lt='ls -lht'
---
+++ .config/shell/init.sh
@@ -0,0 +1,2 @@
+source "$HOME/.config/shell/functions.sh"
+source "$HOME/.config/shell/aliases.sh"
